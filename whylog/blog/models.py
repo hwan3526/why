4a85f91351a7d25e8ae7a71328ad829668f2e1bc
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-09-11 13:40:53 +0900

    feat: add model

--- whylog/blog/models.py
+++ whylog/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=50)
+
+class User(models.Model):
+    nickname = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    last_login_date = models.DateTimeField()
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    blog_img = models.TextField()
+    in_private = models.BooleanField()
+    temporary = models.BooleanField()
+    upload_date = models.DateTimeField()
+    count = models.IntegerField()
+
+
+
+
