cdba84c6caa9465a9c55b20569fb90a8e1507019
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-09-11 16:43:07 +0900

    feat: add router

--- whylog/blog/urls.py
+++ whylog/blog/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
+router = DefaultRouter()
+router.register(r'blog', BlogViewSet)
+router.register(r'user', UserViewSet)
+router.register(r'comment', CommentViewSet)
+router.register(r'like', LikeViewSet)
+router.register(r'alram', AlarmViewSet)
 
+urlpatterns = [
+    path('api/', include(router.urls)),
 ]
