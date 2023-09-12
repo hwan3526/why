387b19d47d57eca3b578335be8d7023de8629728
hwan3526 <hwan3526@gmail.com>
2023-09-12 15:20:15 +0900

    feat: add placeholder in login form

--- whylog/blog/forms.py
+++ whylog/blog/forms.py
@@ -5,6 +5,10 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['nickname', 'password']
+        widgets = {
+            'nickname': forms.TextInput(attrs={'placeholder': 'nickname'}),
+            'password': forms.TextInput(attrs={'placeholder': 'password'}),
+        }
 
 class BlogForm(forms.ModelForm):
     class Meta:
