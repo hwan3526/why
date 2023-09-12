848736864bd7c73314b4dbbdd3bac8b0df9f5423
hwan3526 <hwan3526@gmail.com>
2023-09-12 15:10:16 +0900

    feat: add password attr on User model and UserForm

--- whylog/blog/forms.py
+++ whylog/blog/forms.py
@@ -1,5 +1,10 @@
 from django import forms
-from .models import Blog
+from .models import Blog, User
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['nickname', 'password']
 
 class BlogForm(forms.ModelForm):
     class Meta:
