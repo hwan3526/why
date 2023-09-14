18acdc9c631928a95c6b58fdbe182a4b7369fe3c
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-09-15 00:03:36 +0900

    feat: add custom_tag

---
+++ whylog/blog/templatetags/custom_filters.py
@@ -0,0 +1,11 @@
+from django import template
+from bs4 import BeautifulSoup
+
+register = template.Library()
+
+@register.filter
+def remove_img_tags(value):
+    soup = BeautifulSoup(value, 'html.parser')
+    for img_tag in soup.find_all('img'):
+        img_tag.decompose()
+    return str(soup)
