6f3a9cba84f96f1e5c61bff7f7a1ab42cb0567c0
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-09-14 09:13:52 +0900

    feat: fix login, write

--- whylog/blog/views.py
+++ whylog/blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from rest_framework import viewsets
-from .models import *
+from . models import *
 from .serializers import *
 from .forms import BlogForm, UserForm
+from django.contrib.auth import login, logout, authenticate
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
@@ -27,27 +28,69 @@ class LikeViewSet(viewsets.ModelViewSet):
 class AlarmViewSet(viewsets.ModelViewSet):
     queryset = Alarm.objects.all()
     serializer_class = AlarmSerializer
-    
 
+    
 def board(request):
     theme = 'dark'
-    return render(request, 'board.html', {'theme': theme})
+    is_logined = False
+    blog = Blog.objects.all()
+    return render(request, 'board.html', {'theme': theme, "is_logined": is_logined, 'blogs': blog})
 
+def board_view(request):
+    return redirect('board')
 
 def login(request):
-    form = UserForm()
-    return render(request, 'login.html', {'form': form})
+    is_logined = True
+
+    if request.user.is_authenticated:
+        return redirect('blog:board')
+    else:
+        form = UserForm(data=request.POST or None)
+        if request.method == "POST":
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect('blog:board')
+        return render(request, 'board.html', {'form': form, "is_logined":is_logined})
+    # return render(request, 'board.html', {"is_logined":is_logined})
+
+def logout(request):
+    logout(request)
+    return render(request, "board.html", {"is_logined": False})
+
+# def login(request):
+#     form = UserForm()
+#     return render(request, 'login.html', {'form': form})
 
 def write(request):
-    form = BlogForm()
     theme = 'light'
+
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES)
+        if form.is_valid():
+            blog = form.save(commit=False)
+            blog.user = request.user.id
+            title = request.POST['title']
+            content = request.POST['content']
+            in_private = False
+            temporary = False
+            count = 0
+            category_id = 1
+            user_id_id = request.user.id
+            blog = Blog.objects.create(user_id_id=user_id_id, category_id=category_id, in_private=in_private, temporary=temporary, count=count, title=title, content=content)
+            blog.save()
+            return redirect('board_detail', blog_id=blog.id)
+    else:
+        form = BlogForm()
     return render(request, 'write.html', {'form': form, 'theme': theme})
 
-def board_detail(request):
+def board_detail(request, blog_id):
     theme = 'light'
-    return render(request, 'board-detail.html', {'theme': theme})
-
-
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'board-detail.html', {'theme': theme, 'blog': blog})
 
 # def login(request):
 #     if request.method == 'POST':
