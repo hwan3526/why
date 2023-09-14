1130a249efa86fb056d717ae03c765335bf4f49f
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-09-14 19:39:12 +0900

    fix: change editor

--- whylog/whylog/settings.py
+++ whylog/whylog/settings.py
@@ -42,8 +42,10 @@ INSTALLED_APPS = [
 
     'blog',
     'rest_framework',
-    'ckeditor',
-    'ckeditor_uploader',
+    # 'ckeditor',
+    # 'ckeditor_uploader',
+
+    'tinymce',
 ]
 
 MIDDLEWARE = [
@@ -135,16 +137,50 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CKEDITOR_UPLOAD_PATH = 'uploads/'
-CKEDITOR_IMAGE_BACKEND = "pillow" 
-CKEDITOR_UPLOAD_PATH = 'uploads/'
+# CKEDITOR_UPLOAD_PATH = 'uploads/'
+# CKEDITOR_IMAGE_BACKEND = "pillow" 
+# CKEDITOR_UPLOAD_PATH = 'uploads/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media/'
-CKEDITOR_CONFIGS={
-  'default': {
-    'toolbar': 'Full',
-    'height': 400,
-    'width': 500,
-  },
-}
+# CKEDITOR_CONFIGS={
+#   'default': {
+#     'toolbar': 'Full',
+#     'height': 400,
+#     'width': 500,
+#   },
+# }
 LOGOUT_REDIRECT_URL = '/'
+
+
+TINYMCE_JS_URL = os.path.join(STATIC_URL, 'tinymce/tinymce.min.js')
+TINYMCE_DEFAULT_CONFIG = {
+    'cleanup_on_startup': True,
+    'custom_undo_redo_levels': 20,
+    'selector': 'textarea',
+    'theme': 'silver',
+    'plugins': '''
+            textcolor save link image media preview codesample contextmenu
+            table code lists fullscreen  insertdatetime  nonbreaking
+            contextmenu directionality searchreplace wordcount visualblocks
+            visualchars code fullscreen autolink lists  charmap print  hr
+            anchor pagebreak
+            ''',
+    'toolbar1': '''
+            fullscreen preview bold italic underline | fontselect,
+            fontsizeselect  | forecolor backcolor | alignleft alignright |
+            aligncenter alignjustify | indent outdent | bullist numlist table |
+            | link image media | codesample |
+            ''',
+    'toolbar2': '''
+            visualblocks visualchars |
+            charmap hr pagebreak nonbreaking anchor |  code |
+            ''',
+    'contextmenu': 'formats | link image',
+    'menubar': True,
+    'statusbar': True,
+    'theme_advanced_resizing': True,
+    "images_upload_url": "upload_image",
+    'width': '100%',
+    'height': 600
+
+}
