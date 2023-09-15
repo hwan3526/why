4c7274f3e6bfe7aeb0ffbaeae47fa2bb2bca1b4c
Eunha Kang <75615404+tein408@users.noreply.github.com>
2023-09-15 19:01:34 +0900

    fix: change user to auth_user

--- whylog/blog/forms.py
+++ whylog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from .models import Blog, User
+from .models import Blog
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
 
 class UserForm(AuthenticationForm):
     class Meta:
